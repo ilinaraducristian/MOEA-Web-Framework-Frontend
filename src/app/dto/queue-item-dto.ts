9c9d1865d17951d38d545254cbdf122fa9b041f5
Radu Ilina <ilinaraducristian@protonmail.com>
2020-07-05 18:26:20 +0300

    All packages are updated to @latest

--- src/app/dto/queue-item-dto.ts
+++
@@ -1,7 +0,0 @@
-export interface QueueItemDTO {
-  name: string;
-  problem: string;
-  algorithm: string;
-  numberOfEvaluations: number;
-  numberOfSeeds: number;
-}
--- src/app/dto/user-dto.ts
+++
@@ -1,7 +0,0 @@
-export interface RegisterUserDTO {
-  username: string;
-  password: string;
-  email: string;
-  firstName: string;
-  lastName?: string;
-}
